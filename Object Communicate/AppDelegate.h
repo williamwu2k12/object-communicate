44b33923747bcbcdb00b0fd8967903fce3cef9c8
William Wu <williamwu.2k12@gmail.com>
2014-04-16 19:10:42 -0700

    first commit

---
+++ Object Communicate/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  Object Communicate
+//
+//  Created by William Wu on 4/16/14.
+//  Copyright (c) 2014 William Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ListViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ListViewController * LVC;
+
+@end
---
+++ Object Communicate/ListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListViewController.h
+//  Object Communicate
+//
+//  Created by William Wu on 4/16/14.
+//  Copyright (c) 2014 William Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListViewController : UIViewController
+
+@end
