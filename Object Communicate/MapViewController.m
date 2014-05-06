41209a10fdcb417181c498ef4f0f51c4937095a1
William Wu <williamwu.2k12@gmail.com>
2014-05-05 18:53:29 -0700

    added uisegmentedcontrol

--- Object Communicate/MapViewController.m
+++ Object Communicate/MapViewController.m
@@ -9,6 +9,7 @@
 #import "MapViewController.h"
 #import "UIKit/UIKit.h"
 #import "MapKit/MapKit.h"
+#import "CoreLocation/CoreLocation.h"
 
 @interface MapViewController ()
 
@@ -17,6 +18,8 @@
 @implementation MapViewController
 {
     MKMapView * map;
+    CLLocationManager * manager;
+    UISegmentedControl * maptype;
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -34,7 +37,13 @@
     // Do any additional setup after loading the view.
     
     [self initMap];
+    [self initManager];
     [self goToPlace]; // replace this with location of item in future
+    UIButton * button = [UIButton buttonWithType: UIButtonTypeRoundedRect];
+    [button setFrame: CGRectMake(0.0, 30.0, 50.0, 50.0)];
+    [button setTitle: @"TESTING" forState: UIControlStateNormal];
+    [button addTarget: self action: @selector(currentLocation) forControlEvents: UIControlEventTouchUpInside];
+//    [self.view addSubview: button];
 }
 
 - (void) initMap
@@ -42,6 +51,52 @@
     map = [[MKMapView alloc] initWithFrame: self.view.frame];
     [self.view addSubview: map];
     // initialize hybrid, traffic, etc.
+    maptype = [[UISegmentedControl alloc] initWithItems: [NSArray arrayWithObjects: @"Standard", @"Satellite", @"Hybrid", @"Terrain", nil]];
+    [maptype setFrame: CGRectMake(0.35 * [[UIScreen mainScreen] bounds].size.width, 0.05 * [[UIScreen mainScreen] bounds].size.height, 0.6 * [[UIScreen mainScreen] bounds].size.width, 0.05 * [[UIScreen mainScreen] bounds].size.height)];
+    [maptype setTitleTextAttributes: [NSDictionary dictionaryWithObject: [UIFont fontWithName: @"Verdana" size: 9.0] forKey: NSFontAttributeName] forState: UIControlStateNormal];
+//    [maptype setBackgroundColor: [UIColor lightGrayColor]];
+//    [[maptype layer] setBorderColor: [[UIColor blackColor] CGColor]];
+    [maptype setTintColor: [UIColor blackColor]];
+    [maptype addTarget: self action: @selector(changeMaptype) forControlEvents: UIControlEventValueChanged];
+    [maptype setSelectedSegmentIndex: 0];
+    
+    [self.view addSubview: maptype];
+}
+
+- (void) changeMaptype
+{
+    switch ([maptype selectedSegmentIndex])
+    {
+        case 0:
+        {
+            [map setMapType: MKMapTypeStandard];
+            break;
+        }
+        case 1:
+        {
+            [map setMapType: MKMapTypeSatellite];
+            break;
+        }
+        case 2:
+        {
+            [map setMapType: MKMapTypeHybrid];
+            break;
+        }
+        case 3:
+        {
+            [map setMapType: 3];
+            break;
+        }
+    }
+}
+
+- (void) initManager
+{
+    manager = [[CLLocationManager alloc] init];
+    [manager setDelegate: self];
+    [manager setDistanceFilter: kCLDistanceFilterNone];
+    [manager setDesiredAccuracy: kCLLocationAccuracyBest];
+    [manager startUpdatingLocation];
 }
 
 - (void) goToPlace
@@ -56,6 +111,21 @@
     region.center = coordinate;
     region.span = span;
 
+    [map setRegion: region animated: YES];
+}
+
+- (void) currentLocation
+{
+    CLLocationCoordinate2D coordinate;
+    coordinate.latitude = manager.location.coordinate.latitude;
+    coordinate.longitude = manager.location.coordinate.longitude;
+    MKCoordinateSpan span;
+    span.latitudeDelta = 0.2;
+    span.longitudeDelta = 0.2;
+    MKCoordinateRegion region;
+    region.center = coordinate;
+    region.span = span;
+    
     [map setRegion: region animated: YES];
 }
 
